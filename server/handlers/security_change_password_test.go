ece50aa6826f4446225d001f3e65557457abaf70
Vladimir Teplov <vladimir.teplov@gmail.com>
2023-07-26 19:57:51 +0300

    add change password test

--- server/handlers/security_change_password_test.go
+++ server/handlers/security_change_password_test.go
@@ -11,27 +11,34 @@ import (
 	"testing"
 )
 
-func Test_ForgotPassword(t *testing.T) {
+func Test_ChangePassword(t *testing.T) {
 	ts := initApp()
 	defer ts.Close()
 	db := store.GetDB()
-	registerResp, email, _ := registerUser(t, ts)
+	registerResp, email, password := registerUser(t, ts)
 
 	registeredUser := checkToken(t, db, registerResp.Data.Token)
 	assert.Equal(t, registeredUser.Email, email)
 
-	body := dto.ForgotPassword{
-		Email: email,
+	loggedInUserToken := loginUser(t, ts, email, password)
+	loggedInUser := checkToken(t, db, loggedInUserToken)
+	assert.Equal(t, loggedInUser.Email, registeredUser.Email)
+
+	newPassword := "12345690"
+	body := dto.ChangePassword{
+		OldPassword: password,
+		NewPassword: newPassword,
 	}
 	bodyBytes, err := json.Marshal(body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req, err := http.NewRequest(http.MethodPost, ts.URL+"/api/v1/security/forgot-password", bytes.NewReader(bodyBytes))
+	req, err := http.NewRequest(http.MethodPost, ts.URL+"/api/v1/security/change-password", bytes.NewReader(bodyBytes))
 	if err != nil {
 		t.Fatalf("Failed to create request: %v", err)
 	}
+	req.Header.Set("Authorization", loggedInUserToken)
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
